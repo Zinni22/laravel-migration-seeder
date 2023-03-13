2eded25aa364b4816a0b2160634b41c131b9dac7
Zineb Atlassi <atlassizineb@gmail.com>
2023-03-13 19:15:28 +0100

    Inizializzazione file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel 9 + Bootstrap Template</title>
+        <title>Laravel Migration Serve</title>
 
         {{-- Includiamo gli assets con la direttiva @vite --}}
         @vite('resources/js/app.js')
@@ -12,16 +12,16 @@
     <body>
 
         <main>
+            <h1 class="text-center m-5">
+                Trains
+            </h1>
+
             <div class="container">
                 <div class="row">
                     <div class="col text-center">
 
                         <div class="card">
-                            <div class="card-body">
-                                <img src="{{ Vite::asset('resources/img/laravel.png') }}" alt="">
-
-                                <h1 class="card-title">Laravel 9 + Bootstrap Template</h1>
-                            </div>
+                           
                         </div>
 
                     </div>
